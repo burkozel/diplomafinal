49987e369d9f9ebf569a840851839f9e662b2bc6
burkozel <71974079+burkozel@users.noreply.github.com>
2022-08-24 22:22:15 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -194,3 +194,39 @@ class OrderItem(models.Model):
         verbose_name = 'Заказанная позиция'
         verbose_name_plural = "Список заказанных позиций"
         constraints = [models.UniqueConstraint(fields=['order_id', 'product_info'],name='unique_order_item'),]
+        
+class ConfirmEmailToken(models.Model):
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+
+    @staticmethod
+    def generate_key():
+        return get_token_generator().generate_token()
+
+    user = models.ForeignKey(
+        User,
+        related_name='confirm_email_tokens',
+        on_delete=models.CASCADE,
+        verbose_name=_("The User which is associated to this password reset token")
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("When was this token generated")
+    )
+
+    key = models.CharField(
+        _("Key"),
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(ConfirmEmailToken, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.key
